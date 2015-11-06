3c4a892c997ca6fa142e16aada1e1fc6a8a08a03
Beachman4 <beachman4@hotmail.com>
2015-11-06 08:50:53 -0600

    Updated Database structure with test values

--- database/task.sql
+++ database/task.sql
@@ -1,14 +1,89 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               5.6.25-log - MySQL Community Server (GPL)
+-- Server version:               5.6.26-log - MySQL Community Server (GPL)
 -- Server OS:                    Win64
--- HeidiSQL Version:             9.2.0.4947
+-- HeidiSQL Version:             9.3.0.4984
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table task.categories
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` tinytext,
+  `description` tinytext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table task.categories: ~4 rows (approximately)
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` (`id`, `title`, `description`) VALUES
+	(1, 'Sports', 'Anything Sports goes here'),
+	(2, 'Gaming', 'Anything Gaming Goes here'),
+	(3, 'IT', 'Anything about IT Goes Here'),
+	(4, 'Hardware', 'Anything about hardware goes here');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+
+
+-- Dumping structure for table task.login_attempts
+CREATE TABLE IF NOT EXISTS `login_attempts` (
+  `id` int(11) DEFAULT NULL,
+  `time` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table task.login_attempts: ~0 rows (approximately)
+/*!40000 ALTER TABLE `login_attempts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `login_attempts` ENABLE KEYS */;
+
+
+-- Dumping structure for table task.posts
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `body` longtext,
+  `user_id` int(11) DEFAULT NULL,
+  `thread_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table task.posts: ~6 rows (approximately)
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` (`id`, `body`, `user_id`, `thread_id`) VALUES
+	(1, 'Testing', 1, 1),
+	(2, 'Testing', 1, 1),
+	(3, 'Testing', 1, 2),
+	(4, 'Testing', 1, 2),
+	(5, 'Testing', 1, 3),
+	(6, 'Testing', 1, 3);
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+
+
+-- Dumping structure for table task.threads
+CREATE TABLE IF NOT EXISTS `threads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` tinytext,
+  `body` longtext,
+  `category_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table task.threads: ~8 rows (approximately)
+/*!40000 ALTER TABLE `threads` DISABLE KEYS */;
+INSERT INTO `threads` (`id`, `title`, `body`, `category_id`, `user_id`) VALUES
+	(1, 'Test', 'Testing', 1, 1),
+	(2, 'Test', 'Testing', 1, 1),
+	(3, 'Test', 'Testing', 2, 1),
+	(4, 'Test', 'Testing', 2, 1),
+	(5, 'Test', 'Testing', 3, 1),
+	(6, 'Test', 'Testing', 3, 1),
+	(7, 'Test', 'Testing', 4, 1),
+	(8, 'Test', 'Testing', 4, 1);
+/*!40000 ALTER TABLE `threads` ENABLE KEYS */;
+
 
 -- Dumping structure for table task.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -22,10 +97,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
--- Dumping data for table task.users: ~0 rows (approximately)
+-- Dumping data for table task.users: ~3 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `admin`, `readonly`) VALUES
+	(1, 'MasterYodA', 'beachman4@hotmail.com', 'Lennar@123', 1, 0),
+	(3, 'test', 'test@test.com', 'lennar', 0, 0),
+	(8, 'test123', 'test@test.comasd', 'Lennar', 0, 0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
